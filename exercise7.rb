6c9429645a0a5ab8dbc7024f289a28be072a86d1
lauranjansen <lauran.jansen@gmail.com>
2015-01-21 15:24:25 -0500

    Increase cohort size and delete cohort2

--- exercise7.rb
+++ exercise7.rb
@@ -6,8 +6,23 @@ students = {
 
 def displash(hsh)
 	hsh.each do |cohort,studnum|
-		puts "#{cohort.capitalize} had #{studnum} students."
+		puts "#{cohort.capitalize} has #{studnum} students."
 	end
+	puts
 end
+
+displash(students)
+
+students[:cohort4] = 43
+
+students.keys.each { |key| puts "#{key.capitalize}" }
+
+students.each_key do |key|
+	students[key] = (students[key] * 1.05).round
+end
+
+displash(students)
+
+students.delete(:cohort2)
 
 displash(students)
