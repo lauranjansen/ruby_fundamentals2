3c447b160a75750b546529e70c703a16bb8c973e
lauranjansen <lauran.jansen@gmail.com>
2015-01-21 15:08:21 -0500

    Display info

--- exercise7.rb
+++ exercise7.rb
@@ -3,3 +3,11 @@ students = {
 	:cohort2 => 42,
 	:cohort3 => 22
 }
+
+def displash(hsh)
+	hsh.each do |cohort,studnum|
+		puts "#{cohort.capitalize} had #{studnum} students."
+	end
+end
+
+displash(students)
