0885441c3e17a98d9a277a282fc8e5fbfd7bfdd1
lauranjansen <lauran.jansen@gmail.com>
2015-01-21 14:09:32 -0500

    Add rice and display again

--- exercise6.rb
+++ exercise6.rb
@@ -3,3 +3,9 @@ grocery_list = ["yoghurt", "bananas", "cereal", "beer", "snacks"]
 grocery_list.each do |item|
 	puts "* #{item}"
 end
+
+grocery_list << "rice"
+
+grocery_list.each do |item|
+	puts "* #{item}"
+end
